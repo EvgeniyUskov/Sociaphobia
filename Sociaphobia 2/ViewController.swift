169b8565a73c84cdeae71d633f734966a8967de0
Evgeniy Uskov <driftrboy@gmail.com>
2020-03-30 12:11:32 +0700

    Initial Commit

---
+++ Sociaphobia 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sociaphobia 2
+//
+//  Created by Evgeniy Uskov on 30.03.2020.
+//  Copyright © 2020 Evgeniy Uskov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
